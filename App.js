38a5f3f35dec99dc97b0142f0b0fa76d3b383036
Nikhil Singh <Singh.nikhil999@gmail.com>
2018-03-20 11:25:26 +0530

    added styles :zap:

--- App.js
+++ App.js
@@ -1,24 +1,13 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Header from './Header.js'
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import { Header } from './src/components';
 
-export default class App extends React.Component {
+export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View>
+        <Header />
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
